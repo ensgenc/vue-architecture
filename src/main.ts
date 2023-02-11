6345f18d905ea8b5654ad23b94e9a0555df93ba3
Enes GENÇ <enes.genc23@gmail.com>
2023-02-11 15:20:14 +0300

    created base Input Component

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import router from "./router";
 import store from "./store";
 import Button from "@/components/base/Button.vue";
 import Card from "@/components/base/Card.vue";
+import InputText from "@/components/base/InputText.vue";
 
 const app = createApp(App);
 app.component("PButton", Button);
 app.component("PCard", Card);
+app.component("PInputText", InputText);
 
 app.use(store).use(router).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,10 @@
   <div class="container">
     <p-card class="w-25">
       <template #content>
+        <div class="form-group">
+          <p-input-text id="username" type="text" required />
+          <label>Adınız Soyadınız</label>
+        </div>
         <p-button label="Giriş Yap" class="btn-primary w-100" />
       </template>
     </p-card>
